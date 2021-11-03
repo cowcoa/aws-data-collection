20c6f841654b678c1b5beb50d8dafb1b5114e2cf
EC2 Default User <ec2-user@ip-172-31-13-29.ec2.internal>
2021-11-03 08:54:57 +0000

    fix bug that failed to boostrap s3 bucket in us-east-1.

--- bootstrapping/bootstrap.sh
+++ bootstrapping/bootstrap.sh
@@ -9,7 +9,12 @@ source $SHELL_PATH/config.sh
 # Create deployment s3 bucket if no such bucket.
 if aws s3 ls "s3://$s3_deployment_bucket" 2>&1 | grep -q 'NoSuchBucket'; then
   echo "Creating deployment s3 bucket $s3_deployment_bucket"
-  aws s3api create-bucket --bucket $s3_deployment_bucket --region $deployment_region --create-bucket-configuration LocationConstraint=$deployment_region
+  if [ $deployment_region = us-east-1 ]; then
+    aws s3api create-bucket --bucket $s3_deployment_bucket --region $deployment_region
+  else
+    aws s3api create-bucket --bucket $s3_deployment_bucket --region $deployment_region --create-bucket-configuration LocationConstraint=$deployment_region
+  fi
+ 
   aws s3api put-public-access-block \
     --bucket $s3_deployment_bucket \
     --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
