062a8ec4c18e4a81cd0119caed8f29e485364037
EC2 Default User <ec2-user@ip-172-31-43-205.us-west-2.compute.internal>
2021-12-30 11:49:55 +0000

    add a meaningful error message when the bucket name is occupied during bootstrapping.

--- bootstrapping/bootstrap.sh
+++ bootstrapping/bootstrap.sh
@@ -6,6 +6,12 @@ SHELL_PATH=$(cd "$(dirname "$0")";pwd)
 # Import global variables.
 source $SHELL_PATH/config.sh
 
+# If bucket name is already occupied by another user, CLI 'aws s3 ls' will report 'Access Denied'.
+if aws s3 ls "s3://$s3_deployment_bucket" 2>&1 | grep -q 'Access Denied'; then
+  echo "Bucket $s3_deployment_bucket already exists, please change bucket name."
+  exit -1
+fi
+
 # Create deployment s3 bucket if no such bucket.
 if aws s3 ls "s3://$s3_deployment_bucket" 2>&1 | grep -q 'NoSuchBucket'; then
   echo "Creating deployment s3 bucket $s3_deployment_bucket"
